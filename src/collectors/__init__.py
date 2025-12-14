c76970a7a745eb0eb76e23827ec25ac9179306d8
김우정 <woojung@gim-ujeong-ui-MacBookPro.local>
2025-12-15 02:29:51 +0900

    Fix: remove Reddit import until API approval

--- src/collectors/__init__.py
+++ src/collectors/__init__.py
@@ -1,5 +1,5 @@
-from .reddit import RedditCollector
+# from .reddit import RedditCollector  # Reddit API 승인 후 활성화
 from .hackernews import HackerNewsCollector
 from .rss import RSSCollector
 
-__all__ = ['RedditCollector', 'HackerNewsCollector', 'RSSCollector']
+__all__ = ['HackerNewsCollector', 'RSSCollector']
